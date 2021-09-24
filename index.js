a862bce106704e1a1379e86d0ccc9ca074e63f85
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2021-09-24 11:44:59 -0300

    URL do banco e porta atualizadas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 (async () => {
-    const url = "mongodb://localhost:27017";
+    const url = "mongodb+srv://admin:OM08aMTuOOkQlfAI@cluster10.zi64a.mongodb.net/";
     const dbName = "ocean_bancodados_22_09_2021";
 
     const client = await MongoClient.connect(url);
@@ -140,5 +140,5 @@ const app = express();
         res.send("Personagem removida com sucesso!");
     });
 
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
 })();
